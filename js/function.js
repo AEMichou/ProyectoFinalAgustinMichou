4f0bdc781ffb1d7fe798770c5e2e2fe48bd87eb3
unknown <agustinmichou@gmail.com>
2023-02-28 22:47:50 -0300

    first commit

---
+++ js/function.js
@@ -0,0 +1,4 @@
+document.getElementById("BOTóN").onclick = function () {
+    console.log("Escuchamos una buena rola");
+    document.getElementById("demo").innerHTML = "Estamos meneandola intensamente";
+}

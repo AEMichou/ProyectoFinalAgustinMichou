d32fc79f127c23837330d35c71241e87dde2ae0c
AEMichou <126632233+AEMichou@users.noreply.github.com>
2023-02-28 23:21:54 -0300

    Delete js directory

--- js/function.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("BOTóN").onclick = function () {
-    console.log("Escuchamos una buena rola");
-    document.getElementById("demo").innerHTML = "Estamos meneandola intensamente";
-}
